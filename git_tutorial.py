c9447252838806304b8bac78a120f4ce2891c6ba
“Ken <kcalhoon20@gmail.com>
2024-11-12 09:13:53 -0800

    updated git_tutorial

--- git_tutorial.py
+++ git_tutorial.py
@@ -2,3 +2,11 @@ def add_numbers(a, b):
     return a + b
 
 print(add_numbers(1, 2))
+
+# adding in some extra comments in the master branch
+
+
+def subtract_numbers(a,b):
+    return a - b
+
+"""adding a doc string to this from within feature/docs"""
